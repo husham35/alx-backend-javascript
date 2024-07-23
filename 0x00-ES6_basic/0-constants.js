0c4f6cd189e1e171b6af5c5477fcdbaaa639b4a8
husham35 <husham35@gmail.com>
2024-07-23 04:32:07 +0000

    fixes for task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,17 @@
 /* Modify variables to use const and let */
 
 export function taskFirst () {
-  const task = 'I prefer const when I can.';
-  return task;
+	const task = 'I prefer const when I can.';
+	return task;
 }
 
 export function getLast () {
-  return ' is okay';
+	return ' is okay';
 }
 
 export function taskNext () {
-  let combination = 'But sometimes let';
-  combination += getLast();
+	let combination = 'But sometimes let';
+	combination += getLast();
 
-  return combination;
+	return combination;
 }
