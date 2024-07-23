4ff72b85b7d0ef091d19ba2a88c2c7c61b62e204
husham35 <husham35@gmail.com>
2024-07-23 05:23:07 +0000

    requirements fixes task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,8 @@
-/* Modify the following function’s budget object to simply use the keyname instead. */
-export default function getBudgetObject (income, gdp, capita) {
+export default function getBudgetObject(income, gdp, capita) {
 	const budget = {
 		income,
 		gdp,
-		capita
+		capita,
 	};
 
 	return budget;
