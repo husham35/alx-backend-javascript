97aad9bdcd0133ced1f8c790f7bfad2878f42d0e
husham35 <husham35@gmail.com>
2024-07-23 11:06:24 +0000

    more fixes

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,3 @@
-/* write a function named iterateThroughObject. The function’s parameter reportWithIterator is the return value from createIteratorObject. */
 export default function iterateThroughObject(reportWithIterator) {
 	return [...reportWithIterator].join(' | ');
 }
