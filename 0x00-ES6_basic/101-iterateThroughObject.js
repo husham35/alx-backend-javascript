2771016416cb82dae4f39f73695ddc37a9ec650d
husham35 <husham35@gmail.com>
2024-07-23 04:54:19 +0000

    task 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,5 @@
 /* write a function named iterateThroughObject. The function’s parameter reportWithIterator is the return value from createIteratorObject. */
 
 export default function iterateThroughObject(reportWithIterator) {
-	const employees = [];
-
-	for (const employee of reportWithIterator) {
-		employees.push(employee);
-	}
-
-	return employees.join(' | ');
+  return [...reportWithIterator].join(' | ');
 }
